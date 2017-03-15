c1188e11a2a989224a2c64f5bef992e3322f35b6
AndyPhoenix9879 <Lai9879M@gmail.com>
2017-03-15 02:39:30 -0700

    Some modifications.. Thanks dani020110 - helped me got into zip making

--- packzip.sh
+++ packzip.sh
@@ -2,10 +2,10 @@
 set -e
 LGREEN='\033[1;32m'
 
-KERNEL_UNSIGNED=destiny_r3-unsigned.zip
-KERNEL_FALSE_SIGNED=destiny_r3-false_signed.zip
-KERNEL_ADJUSTED_UNSIGNED=destiny_r3-adjusted_unsigned.zip
-KERNEL_NAME=destiny_r3.zip
+KERNEL_UNSIGNED=v6-unsigned.zip
+KERNEL_FALSE_SIGNED=v6-false_signed.zip
+KERNEL_ADJUSTED_UNSIGNED=v6_unsigned.zip
+KERNEL_NAME=v6.zip
 cd zipme
 zip -r ../$KERNEL_UNSIGNED .
 cd ..
@@ -14,7 +14,7 @@ java -Xmx2048m -jar signing/signapk.jar -w signing/testkey.x509.pem signing/test
 rm -f $KERNEL_UNSIGNED
 signing/zipadjust $KERNEL_FALSE_SIGNED $KERNEL_ADJUSTED_UNSIGNED
 rm -f $KERNEL_FALSE_SIGNED
-java -Xmx2048m -jar signing/minsignapk.jar signing/testkey.x509.pem signing/testkey.pk8 $KERNEL_ADJUSTED_UNSIGNED RELEASE/$KERNEL_NAME
+java -Xmx2048m -jar signing/minsignapk.jar signing/testkey.x509.pem signing/testkey.pk8 $KERNEL_ADJUSTED_UNSIGNED $KERNEL_NAME
 rm -f $KERNEL_ADJUSTED_UNSIGNED
 
 echo -e "${LGREEN}Destiny kernel sucessfully packed and signed as ${KERNEL_NAME}!${NCOLOR}"
